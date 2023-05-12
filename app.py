eb329f4752b9a2209550376a284ba1ca11230c4b
Jonathan Wang <jonathan.s.wang@berkeley.edu>
2023-05-12 02:35:31 -0700

    alternative prompt

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ client_secret = os.getenv('CLIENT_SECRET')
 # scope = 'read_all,activity:read_all'
 redirect_uri = 'localhost:5000'
 access_token = None
+athlete_info = dict()
 
 
 @app.route('/')
@@ -25,7 +26,7 @@ def index():
     redirect_uri = 'http://localhost:5000/exchange_token'
     scope = 'activity:read_all,read_all,profile:read_all'
     if access_token:
-        return redirect(url_for('workout_index'))
+        return redirect(url_for('collect_athlete_data'))
     return redirect(f"http://www.strava.com/oauth/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&approval_prompt=force&scope={scope}")
 
 
@@ -41,66 +42,82 @@ def strava_callback():
 
         session['access_token'] = access_token
         # Redirect the user to a new page
-        return redirect(url_for('workout_index'))
+        return redirect(url_for('collect_athlete_data'))
     except Exception:
         return render_template("redirect_authorization.html")
 
 
-@app.route("/workout_index", methods=("GET", "POST"))
-def workout_index():
+@app.route('/collect_athlete_data')
+def collect_athlete_data():
     access_token = session.get('access_token')
     if not access_token:
         # If the access token is missing, redirect the user to the authorization page
         return redirect('/')
 
-    athlete_info = get_athlete(access_token)
-    athleteId = athlete_info['id']
+    athlete_info["athlete"] = get_athlete(access_token)
 
-    athlete_stats = get_athlete_stats(athleteId, access_token)
+    athlete_info["athlete_stats"] = get_athlete_stats(athlete_info["athlete"]['id'], access_token)
 
-    athlete_activities = clean_activity_json(get_athlete_activities(access_token))
+    athlete_info["athlete_activities"] = clean_activity_json(get_athlete_activities(access_token))
 
-    route_suggestions = get_route_suggestions(athlete_activities, access_token)
+    athlete_info["route_suggestions"] = get_route_suggestions(athlete_info["athlete_activities"], access_token)
+
+    session['count'] = 0
+
+    return redirect(url_for("workout_index"))
+
+
+@app.route("/workout_index", methods=("GET", "POST"))
+def workout_index():
+    athlete_stats = athlete_info["athlete_stats"]
+
+    athlete_activities = athlete_info["athlete_activities"]
+
+    route_suggestions = athlete_info["route_suggestions"]
+
+    try:
+        with open('data.json', 'r') as file:
+            conversation = json.load(file)
+    except FileNotFoundError:
+        print('Your conversation was lost.')
 
     if request.method == "POST":
-        count = request.form.get('count', 0, type=int)
-        count += 1
+        count = session.get('count')
         # Load the previous conversation from a file
-        if count == 1:
+        if count == 0:
             goal = request.form["goal"]
             prompt = generate_prompt(goal, athlete_stats, athlete_activities, route_suggestions)
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=prompt,
+            entry = {"role": "user", "content": prompt}
+            conversation.append(entry)
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=conversation,
                 temperature=0.6,
             )
-
             # Open the file in append mode
-            with open('conversation/conversation.txt', 'a') as f:
-                # Append the response to the file
-                f.write(response.choices[0].text + '\n')
+            with open('conversation/conversation.json', 'a') as file:
+                json.dump(conversation, file)
 
-            return redirect(url_for("index", result=response.choices[0].text))
-        else:
-            try:
-                with open('conversation/conversation.txt', 'r') as file:
-                    conversation = file.read()
-            except FileNotFoundError:
-                print('Your conversation was lost.')
-            question = request.form["Goal"]
-            prompt = generate_conversation_prompt(question, athlete_stats, athlete_activities, conversation)
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=prompt,
-                temperature=0.6,
-            )
+            session['count'] += 1
 
-            # Open the file in append mode
-            with open('conversation/conversation.txt', 'a') as f:
-                # Append the response to the file
-                f.write(response.choices[0].text + '\n')
+            return redirect(url_for("workout_index", result=response.choices[0].text))
+        # else:
+        #     question = request.form["Goal"]
+        #     entry = {"role": "user", "content": }
+        #     conversation.append(entry)
+        #     prompt = generate_conversation_prompt(question, athlete_stats, athlete_activities, conversation)
+        #     response = openai.ChatCompletion.create(
+        #         model="gpt-3.5-turbo",
+        #         prompt=prompt,
+        #         temperature=0.6,
+        #     )
 
-            return redirect(url_for("index", result=response.choices[0].text))
+            # # Open the file in append mode
+            # with open('conversation/conversation.txt', 'a') as f:
+            #     # Append the response to the file
+            #     f.write(response.choices[0].text + '\n')
+            #
+            # return redirect(url_for("workout_index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
